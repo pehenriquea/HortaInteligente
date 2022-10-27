5c0d582088e2013a180032935678a70365fb66ef
Pedro Santos <99020483+pehenriquea@users.noreply.github.com>
2022-10-27 09:15:02 -0300

    Create mqtt_umidadeSolo_semTeste.ino

---
+++ mqtt_umidadeSolo_semTeste.ino
@@ -0,0 +1,231 @@
+#include <arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <DHT.h>
+
+/*Define do Código*/
+//Define Sensor DHT
+#define DHTPIN 12
+#define DHTTYPE DHT11
+
+//Define Relé/BLINK
+#define LED_BUILTIN 2
+#define PIN_LED 2
+
+//Declaração variáveis Sensor de Umidade do Solo
+#define sensorUS 14
+int valorLido; //VARIÁVEL QUE ARMAZENA O PERCENTUAL DE UMIDADE DO SOLO
+int analogSoloSeco = 400; //VALOR MEDIDO COM O SOLO SECO
+int analogSoloMolhado = 150; //VALOR MEDIDO COM O SOLO MOLHADO
+int percSoloSeco = 0;
+int percSoloMolhado = 100;
+
+/* Definicoes para o MQTT */
+#define TOPICO_SUBSCRIBE_LED         "topico_liga_desliga_led"
+#define TOPICO_PUBLISH_TEMPERATURA   "topico_publica_temp"
+#define ID_MQTT  "IoT_PUC_SG_mqtt"     //id mqtt (para identificação de sessão)
+
+//A50 Pedro
+const char* SSID     = "Galaxy A50812D"; // SSID / nome da rede WI-FI que deseja se conectar
+const char* PASSWORD = "36393525"; // Senha da rede WI-FI que deseja se conectar
+const char* BROKER_MQTT = "test.mosquitto.org";
+int BROKER_PORT = 1883; // Porta do Broker MQTT
+
+/*//Casa Pedro
+const char* SSID     = "Justweb-Pedro"; // SSID / nome da rede WI-FI que deseja se conectar
+const char* PASSWORD = "36393525"; // Senha da rede WI-FI que deseja se conectar
+const char* BROKER_MQTT = "test.mosquitto.org";
+int BROKER_PORT = 1883; // Porta do Broker MQTT*/
+
+//Variáveis e objetos globais
+WiFiClient espClient; // Cria o objeto espClient
+PubSubClient MQTT(espClient); // Instancia o Cliente MQTT passando o objeto espClient
+DHT dht(DHTPIN, DHTTYPE);
+
+/* Prototypes */
+void initWiFi(void);
+void initMQTT(void);
+void mqtt_callback(char* topic, byte* payload, unsigned int length);
+void reconnectMQTT(void);
+void reconnectWiFi(void);
+void VerificaConexoesWiFIEMQTT(void);
+void umidadeSolo();
+void umidadeTemp();
+
+/*
+   Implementações
+*/
+/* Função: inicializa e conecta-se na rede WI-FI desejada
+   Parâmetros: nenhum
+   Retorno: nenhum
+*/
+void initWiFi(void)
+{
+  delay(10);
+  Serial.println("------Conexao WI-FI------");
+  Serial.print("Conectando-se na rede: ");
+  Serial.println(SSID);
+  Serial.println("Aguarde");
+  reconnectWiFi();
+}
+/* Função: inicializa parâmetros de conexão MQTT(endereço do broker, porta e seta função de callback)
+   Parâmetros: nenhum
+   Retorno: nenhum
+*/
+void initMQTT(void)
+{
+  MQTT.setServer(BROKER_MQTT, BROKER_PORT);   //informa qual broker e porta deve ser conectado
+  MQTT.setCallback(mqtt_callback);            //atribui função de callback (função chamada quando qualquer informação de um dos tópicos subescritos chega)
+}
+
+/* Função: função de callback
+           esta função é chamada toda vez que uma informação de
+           um dos tópicos subescritos chega)
+   Parâmetros: nenhum
+   Retorno: nenhum
+*/
+void mqtt_callback(char* topic, byte* payload, unsigned int length)
+{
+  String msg;
+
+  /* obtem a string do payload recebido */
+  for (int i = 0; i < length; i++)
+  {
+    char c = (char)payload[i];
+    msg += c;
+  }
+  Serial.print("Chegou a seguinte string via MQTT: ");
+  Serial.println(msg);
+
+  /* toma ação dependendo da string recebida */
+  if (msg.equals("L"))
+  {
+    digitalWrite(PIN_LED, HIGH);
+    Serial.println("LED aceso mediante comando MQTT");
+  }
+  if (msg.equals("D"))
+  {
+    digitalWrite(PIN_LED, LOW);
+    Serial.println("LED apagado mediante comando MQTT");
+  }
+}
+/*  Função:  reconecta-se  ao  broker  MQTT  (caso  ainda  não  esteja  conectado  ou  em  caso  de  a 
+conexão cair)
+  em caso de sucesso na conexão ou reconexão, o subscribe dos tópicos é refeito.
+  Parâmetros: nenhum
+  Retorno: nenhum
+*/
+void reconnectMQTT(void)
+{
+  while (!MQTT.connected())
+  {
+    Serial.print("* Tentando se conectar ao Broker MQTT: ");
+    Serial.println(BROKER_MQTT);
+    if (MQTT.connect(ID_MQTT))
+    {
+      Serial.println("Conectado com sucesso ao broker MQTT!");
+      MQTT.subscribe(TOPICO_SUBSCRIBE_LED);  //topicos inscritos para receber informações
+    }
+    else
+    {
+      Serial.println("Falha ao reconectar no broker.");
+      Serial.println("Havera nova tentativa de conexao em 2s");
+      delay(2000);
+    }
+  }  
+}
+/* Função: verifica o estado das conexões WiFI e ao broker MQTT.
+           Em caso de desconexão (qualquer uma das duas), a conexão
+           é refeita.
+   Parâmetros: nenhum Retorno: nenhum
+*/
+void VerificaConexoesWiFIEMQTT(void)
+{
+  if (!MQTT.connected())
+    reconnectMQTT(); //se não há conexão com o Broker, a conexão é refeita
+    reconnectWiFi(); //se não há conexão com o WiFI, a conexão é refeita
+}
+/* Função: reconecta-se ao WiFi
+   Parâmetros: nenhum
+   Retorno: nenhum
+*/
+void reconnectWiFi(void)
+{
+  //se já está conectado a rede WI-FI, nada é feito.
+  //Caso contrário, são efetuadas tentativas de conexão
+  if (WiFi.status() == WL_CONNECTED)
+    return;
+  WiFi.begin(SSID, PASSWORD); // Conecta na rede WI-FI
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Conectado com sucesso na rede ");
+  Serial.print(SSID);
+  Serial.println("\nIP obtido: ");
+  Serial.println(WiFi.localIP());
+}
+
+void umidadeSolo() {
+  //MANTÉM valorLido DENTRO DO INTERVALO (ENTRE analogSoloMolhado E analogSoloSeco)
+  valorLido = constrain(analogRead(sensorUS), analogSoloMolhado, analogSoloSeco);
+
+  //EXECUTA A FUNÇÃO "map" DE ACORDO COM OS PARÂMETROS PASSADOS
+  valorLido = map(valorLido, analogSoloMolhado, analogSoloSeco, percSoloMolhado, percSoloSeco);
+  Serial.print("Umidade do solo: ");
+  Serial.print(valorLido);
+  Serial.println("%");
+  delay(1000);
+}
+
+void umidadeTemp(){
+  char temperatura_str[13] = {0};
+  float h = dht.readHumidity();
+  long t = dht.readTemperature();
+
+  if (isnan(t)||isnan(h)) {
+    Serial.println("Failed to read from DHT");
+  } else {
+    Serial.print("Umidade: ");
+    Serial.print(h);
+    Serial.print("%");
+    Serial.println();
+    Serial.print("Temperatura: ");
+    Serial.print(t);
+    Serial.println("*C");
+  }
+
+  /*converte a temperatura lida para string*/
+  sprintf(temperatura_str, "%dC", t);
+
+  /*  Publica a temperatura */
+  MQTT.publish(TOPICO_PUBLISH_TEMPERATURA, temperatura_str);
+  Serial.println(temperatura_str);
+}
+
+void setup() {
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println("Disciplina IoT: acesso a nuvem via ESP32");
+  delay(1000);
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, LOW);
+  initWiFi();
+  initMQTT();
+  dht.begin();
+}
+
+void loop() {
+  /* garante funcionamento das conexões WiFi e ao broker MQTT */
+  VerificaConexoesWiFIEMQTT();
+
+  umidadeSolo();
+  umidadeTemp();
+
+  /* mantém da comunicação com broker MQTT */
+  MQTT.loop();
+  /* Refaz o ciclo após 2 segundos */
+  delay(2000);
+}
